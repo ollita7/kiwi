b2dafe9e0cd851521c77a32d5e45fe556c8baa2f
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-06-02 18:53:29 -0300

    Create vs solution to debug and test First version of node server creation

---
+++ test/server.ts
@@ -0,0 +1,3 @@
+import { createKiwiServer } from '../lib/index';
+const server = createKiwiServer();
+server.listen(8086);
---
+++ test/test-controller.ts
@@ -0,0 +1,18 @@
+
+export class TestController {
+    public get(){
+        return "get test";
+    }
+
+    public post(){
+        return "post test";
+    }
+
+    public put(){
+        return "put test";
+    }
+
+    public delete(){
+        return "delete test";
+    }
+}
