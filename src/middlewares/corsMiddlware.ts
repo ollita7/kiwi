b7fd112c59c0b58478d3fe84bbd1ff87c17598d7
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-09-28 12:46:42 -0300

    Fix issue with corsMiddleware

--- src/middlewares/corsMiddlware.ts
+++ src/middlewares/corsMiddlware.ts
@@ -1,17 +1,16 @@
 import { IMiddleware } from './middleware';
 import * as http from 'http';
 export class CorsMiddleware implements IMiddleware {
-    public execute(request: http.IncomingMessage, response: http.ServerResponse, next: any): any{
+    public execute(request: http.IncomingMessage, response: http.ServerResponse, next: any): any {
         if (request.method === 'OPTIONS') {
-            var headers: any = {};
+            var headers = response.getHeaders();
             headers['Access-Control-Allow-Origin'] = '*';
             headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS';
-            headers['Access-Control-Allow-Credentials'] = false;
-            headers['Access-Control-Max-Age'] = '86400';
-            headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
+            headers['Access-Control-Allow-Headers'] = response.getHeader('Access-Control-Allow-Headers') + ', X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
             response.writeHead(204, headers);
             response.end();
-        } else{
+        }
+        else {
             response.setHeader('Access-Control-Allow-Origin', '*');
             response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
