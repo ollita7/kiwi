bb5f8f139e862df4c56944e750376a140633b3e4
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2019-03-14 12:10:45 -0300

    hotfix: misspeled origin on corsMiddleware

--- src/middlewares/corsMiddlware.ts
+++ src/middlewares/corsMiddlware.ts
@@ -8,7 +8,7 @@ export class CorsMiddleware implements IMiddleware {
         let origin = '';
         if(isNil(domains) || domains.length === 0){
             origin = '*';
-        } else if(!isNil(request.headers.origin) && indexOf(domains, request.headers.orgin) > 0){
+        } else if(!isNil(request.headers.origin) && indexOf(domains, request.headers.origin) > 0){
             origin = request.headers.orgin as string;
         }
         if (request.method === 'OPTIONS') {
