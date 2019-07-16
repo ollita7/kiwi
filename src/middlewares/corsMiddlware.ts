1234ee758ab2bc35879852aab284e4d9c233dbb5
gfernandez@menlo-technologies.com <guillermo.fernandez@cavepot.com>
2019-07-15 22:30:55 -0300

    add test for cors middleware

--- src/middlewares/corsMiddlware.ts
+++ src/middlewares/corsMiddlware.ts
@@ -26,4 +26,4 @@ export class CorsMiddleware implements IMiddleware {
             next();
         }
     }
-} 
+}
---
+++ tests/middlewares.test.ts
@@ -0,0 +1,55 @@
+import { suite, test } from "mocha-typescript";
+import { assert } from 'chai';
+import { CorsMiddleware } from '../src/middlewares/corsMiddlware';
+var httpMocks = require('node-mocks-http');
+
+
+@suite class MIddlewaresSuite {
+  static before() {
+    (global as any).options = { cors: { domains: ['test'] } };
+  }
+
+  before() {
+
+  }
+
+  @test 'It must execute cors middleware for options'() {
+    const corsMiddleware = new CorsMiddleware();
+    const param = 'pepe';
+    var request = httpMocks.createRequest({
+      method: 'OPTIONS',
+      url: `/v1/testcontroller/testinguy/${param}`
+    });
+    var response = httpMocks.createResponse();
+    response.getHeaders = function () {
+      return {};
+    };
+    corsMiddleware.execute(request, response, null);
+    assert.equal(response.statusCode, 204);
+  }
+
+  @test 'It must execute cors middleware for post'() {
+    const corsMiddleware = new CorsMiddleware();
+    const param = 'pepe';
+    var request = httpMocks.createRequest({
+      method: 'POST',
+      url: `/v1/testcontroller/testinguy/${param}`
+    });
+    var response = httpMocks.createResponse();
+    response.getHeaders = function () {
+      return {};
+    };
+    corsMiddleware.execute(request, response, null);
+    //assert.equal(response.statusCode, 200);
+  }
+
+
+
+  static after() {
+
+  }
+
+  after() {
+
+  }
+}
